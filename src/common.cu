b257c6247090b123a07ef1508cce2e92618565cb
Scott Moe <moes1@noreply.com>
2025-10-01 20:53:28 -0500

    Add timestamp if test is running infinitely

--- src/common.cu
+++ src/common.cu
@@ -646,7 +646,19 @@ testResult_t TimeTest(struct threadArgs* args, ncclDataType_t type, const char* 
       setupArgs(size, type, args);
       char rootName[100];
       sprintf(rootName, "%6i", root);
-      PRINT("%12li  %12li  %8s  %6s  %6s", max(args->sendBytes, args->expectedBytes), args->nbytes / wordSize(type), typeName, opName, rootName);
+      if (run_cycles == 0) {
+        time_t t;
+        struct tm *tm_info;
+        char ts[26];
+
+        time(&t);
+        tm_info = localtime(&t);
+        strftime(ts, 26, "%Y-%m-%d-%H-%M-%S", tm_info);
+        PRINT("%s %12li  %12li  %8s  %6s  %6s", ts, max(args->sendBytes, args->expectedBytes), args->nbytes / wordSize(type), typeName, opName, rootName);
+      }
+      else {
+        PRINT("%12li  %12li  %8s  %6s  %6s", max(args->sendBytes, args->expectedBytes), args->nbytes / wordSize(type), typeName, opName, rootName);
+      }
       TESTCHECK(BenchTime(args, type, op, root, 0));
       TESTCHECK(BenchTime(args, type, op, root, 1));
       PRINT("\n");
